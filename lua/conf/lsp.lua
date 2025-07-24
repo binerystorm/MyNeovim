db7cff6d7d29e450e86916f412f6dabf92a49b32
binerystorm <gerick.de.kruyf@gmail.com>
2025-07-24 07:46:37 +0200

    added c lsp in the form of ccls

--- lua/conf/lsp.lua
+++ lua/conf/lsp.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_autocmd('FileType', {
         vim.lsp.start({
             name = 'rusty',
             cmd = {'rust-analyzer'},
-            root_dir = vim.fs.root(ev.buf, {'Cargo.toml'}),
+            root_dir = vim.fs.root(ev.buf, {'main.rs', 'Cargo.toml'}),
         })
     end,
 })
@@ -21,9 +21,20 @@ vim.api.nvim_create_autocmd('FileType', {
         })
     end,
 })
+vim.api.nvim_create_autocmd('FileType', {
+    -- This handler will fire when the buffer's 'filetype' is "python"
+    pattern = 'c',
+    callback = function(ev)
+        vim.lsp.start({
+            name = 'cbaby',
+            cmd = {'ccls'},
+            root_dir = vim.fs.root(ev.buf, {'.ccls', '.git', 'main.c'}),
+        })
+    end,
+})
 
 vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(args)
-        print("helloooo")
+        print("lsp running...")
     end
 })
